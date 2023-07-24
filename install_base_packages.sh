e48d01685766e8895cb8b0ea67bf0c896babca24
Aymen Benbellil <126600806+AymenBenbellil@users.noreply.github.com>
2023-07-24 17:35:06 +0000

    Add files via upload

--- install_base_packages.sh
+++ install_base_packages.sh
@@ -1,3 +1,3 @@
 # /bin/bash
 
-sudo pacman -S xorg xorg-xinit make libxft libxinerama xwallpaper picom pulseaudio pavucontrol mpv sxiv viewnior yt-dlp i3-wm i3blocks qemu-full libvirt virt-manager dnsmasq unp unzip alacritty ttf-jetbrains-mono htop neofetch lxappearance thunar terminus-font zathura zathura-pdf-mupdf nvidia nvidia-utils vde2 ebtables iptables-nft nftables bridge-utils ovmf
+sudo pacman -S xorg xorg-xinit make libxft libxinerama xwallpaper pulseaudio pavucontrol mpv sxiv viewnior yt-dlp i3-wm i3blocks qemu-full libvirt virt-manager dnsmasq unp unzip alacritty ttf-jetbrains-mono htop neofetch lxappearance thunar terminus-font zathura zathura-pdf-mupdf nvidia nvidia-utils vde2 ebtables iptables-nft nftables bridge-utils ovmf ranger
