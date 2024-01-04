371946c5abd82403861f96cab2bb2d0efcd03c19
Aymen Benbellil <126600806+AymenBenbellil@users.noreply.github.com>
2024-01-04 10:03:05 +0100

    Update install_base_packages.sh

--- scripts/install_base_packages.sh
+++ scripts/install_base_packages.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sudo pacman -S xorg xorg-xinit make libxft libxinerama xwallpaper pulseaudio pavucontrol mpv sxiv yt-dlp unp unzip ttf-jetbrains-mono htop neofetch lxappearance thunar terminus-font zathura zathura-pdf-mupdf nvidia nvidia-utils iptables-nft nftables ranger sysstat awesome-terminal-fonts ttf-nerd-fonts-symbols neovim curl tar rsync firefox
+sudo pacman -S xorg xorg-xinit make libxft \
+libxinerama xwallpaper pulseaudio pavucontrol \
+mpv sxiv yt-dlp unp unzip ttf-jetbrains-mono \
+htop neofetch lxappearance thunar terminus-font \
+zathura zathura-pdf-mupdf nvidia nvidia-utils \
+iptables-nft nftables ranger sysstat neovim curl \
+awesome-terminal-fonts ttf-nerd-fonts-symbols tar \
+rsync firefox
